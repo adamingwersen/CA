c2e3798b4849b7400a73ddc698ba7dc2f194285f
Adam Ingwersen <adamlinnemann@gmail.com>
2015-11-05 23:54:55 +0100

    Revision
    Added descriptions
    Added headers
    Removed excessive calls/code
    Facebook oauth key > 2hr token

--- Liberal_CA_FB_Crawler.R
+++ Liberal_CA_FB_Crawler.R
@@ -1,11 +1,15 @@
 ### #### ### #### ### #### ### #### ### #### ### #### ### #### ### #### ### #### ### #### ### #### ### #### ### #### ### #### ### ####
+                                                              #LIB PARTY SITE
 
-#LIB PARTY SITE
-
+                                                    # Scrape ndp party site using rvest #
+                                                    #####################################
+# Define link & css-selectors for fetching basic info on webpage
 testlib.link = "https://www.liberal.ca/mp/"
 css.selector.lib = "div.social > a:nth-child(2)"
 css.selector.lib2 = "h2.name"
 
+# Use rvest to fetch from url
+library("rvest")
 lib.test = read_html(testlib.link, encoding = "UTF-8") %>%
   html_nodes(css = css.selector.lib) %>%
   html_attr(name = 'href')
@@ -13,22 +17,17 @@ lib.test = read_html(testlib.link, encoding = "UTF-8") %>%
 lib2.test = read_html(testlib.link, encoding = "UTF-8") %>%
   html_nodes(css = css.selector.lib2) %>%
   html_text()
-  
-    ## Data Cleaning
 
-head(politwitter.df$politwitter.link, 1)
-
-libface.df = data.frame(lib2.test, lib.test)
-
+                                                  # Cleaning, manipulation / preparation #
+                                                  ########################################
+libface.df = data.frame(lib2.test, lib.test) # Data.frame
+# Remove https
 libface.df$reallink = gsub("\\https://www.facebook.com/", "", libface.df$lib.test)
-libface.df$reallink = gsub("\\?.*$", "", libface.df$reallink)
+libface.df$reallink = gsub("\\?.*$", "", libface.df$reallink) # Remove any ?'s from string
 
-# pages/ - fix, however the pages is still an issue due to wierd code at the end. 
-libface.df$reallink = gsub("\\pages/", "", libface.df$reallink)
-libface.df$reallink = gsub("\\/.*$", "", libface.df$reallink)
 
-### Clean all unqualified, i.e. link contains : ".numeric", "/pages/" or BlANK / NA
-
+# Clean all unqualified, i.e. link contains : ".numeric", "/pages/" or BlANK / NA
+# Unqualified : links that are not structurally viable as feed into getPage-fct.
 libface.df = libface.df[-grep("\\%", libface.df$reallink),]
 libface.df = libface.df[-grep(" ", libface.df$reallink),]
 libface.df$reallink = gsub("\\-", ".", libface.df$reallink)
@@ -37,7 +36,7 @@ libface.df = libface.df[-grep("\\pages", libface.df$lib.test),]
 libface.df = libface.df[-grep("\\http:", libface.df$reallink),]
 libface.df = libface.df[grep("\\[1-9]|[A-z]", libface.df$reallink),]
 
-### Extra exceptions by looping:
+# Invalid pages : 404's, IP/Country-restrictions, !pages -> individuals/persons
 libface.df = libface.df[-grep("\\SeanCaseyCharlottetownMP", libface.df$reallink),]
 libface.df = libface.df[-grep("\\PeterFonsecaMississauga", libface.df$reallink),]
 libface.df = libface.df[-grep("\\geoffreganmp", libface.df$reallink),]
@@ -56,44 +55,18 @@ NameSub = libface.df[ ,'lib2.test']
 # Quotes around text
 message(dQuote(URLSub))
 
-head(URLSub, 10)
+                                                  # Facebook-request & loop using plyr #
+                                                  ######################################
+# Load Facebook oauth into environment:
+load("fb_oauth")
 
-# PLYR LOOP
-
-library("plyr")
-dplr = data.frame(ldply(URLSub, scrapelib_fb, .inform = TRUE))
-
-      ### FACEBOOK FUNCTION
-
-token = "CAACEdEose0cBAPeZAkDZBbVLpMp75lctnChr6uXImNuwv7jRjKcKGn0X891oWdm2368Sf9P8cv7KcUNSN7tRQeZCU9Jz4lN2mj3ZBRDMqExXk2E0Dauus3130iCuzVWF7NhQteFQMdCHlZBM1X29CZAKvslQg73YUk8sYWhCe5XDKUdabKFceQZAI6R0Qer5DGUVXWjwStUZCQZDZD"
-
-
+# Get posts from each element in URLSub (up to 10k) from 1st of july until 19th of october (2015)
 require("Rfacebook")
 scrapelib_fb = function(URLSub){
-  fbsh = getPage(URLSub, token, n = 2)
+  fbsh = getPage(URLSub, token = fb_oauth, n = 10000 since = '2015/07/01', until = '2015/10/19')
   if(class(fbsh)=='try-error') next;
   return(fbsh)
-  }
-
-### MISC
-
-head(URLSub[105])
-
-URLSUBUNO = scrapelib_fb(URLSub[13])
-
-URLSub[13]
-
-
-
-post.lib.hist = data.frame() {
-  for(i in URLSub){
-    print(paste("processing", i, sep = " "))
-    post.lib.hist[[i]] = scrapelib_fb(i)
-    assign(paste0("post.lib.hist", i), i)
-    #wait
-    Sys.sleep(1)
-    cat("done!\n")
-  }
-
-
-write.table(libface.df, "wd", sep = "\t")
+}
+# Iterate through all elements and apply function
+library("plyr")
+dplr = data.frame(ldply(URLSub, scrapelib_fb, .inform = TRUE))
